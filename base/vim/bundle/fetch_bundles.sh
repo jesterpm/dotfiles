6674d294ff99b4f6439e6e7f7a8cde399d2bf507
Jesse Morgan <jesse@jesterpm.net>
2018-03-15 21:03:31 -0700

    New machine. New configs.

---
+++ base/vim/bundle/fetch_bundles.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+##
+## I don't want to include the plugin repos in my dotfiles,
+## so here are the comments to fetch the plugins I care about.
+##
+
+git clone git://github.com/tpope/vim-abolish.git
+git clone https://github.com/ctrlpvim/ctrlp.vim.git
+git clone https://github.com/tomtom/tcomment_vim.git
