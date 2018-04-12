bea366fcdfa72910fde029205b677d28646e5dc4
Jesse Morgan <jesse@jesterpm.net>
2018-04-12 16:00:46 -0700

    Misc. changes from new laptop

--- base/vim/bundle/fetch_bundles.sh
+++ base/vim/bundle/fetch_bundles.sh
@@ -8,3 +8,4 @@
 git clone git://github.com/tpope/vim-abolish.git
 git clone https://github.com/ctrlpvim/ctrlp.vim.git
 git clone https://github.com/tomtom/tcomment_vim.git
+git clone https://github.com/leafgarland/typescript-vim.git
